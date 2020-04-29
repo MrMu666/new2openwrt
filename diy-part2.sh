71dc1f9509d821d0d9da8960b7b2947594df8e81
ZJBooksGit <654274664@qq.com>
2020-04-29 17:24:07 +0800

    11

--- diy-part2.sh
+++ diy-part2.sh
@@ -5,8 +5,10 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 # Lisence: MIT
 # Author: P3TERX
-# Blog: https://p3terx.com
+# Blog: https://p3terx.com 
 #============================================================
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+git clone https://github.com/pexcn/openwrt-hev-socks5-server.git package/hev-socks5-server
